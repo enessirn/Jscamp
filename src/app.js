830c3d36d95d11f079cf9a68fab36e8dc929b76f
Enes Şirin <sefsirin55@gmail.com>
2021-12-30 23:13:55 +0300

    30 Aralık Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba Dünya")
 
-//JS type safe değildir
+// JS type safe değildir
 let dolarBugun = 9.30
 
 let dolarDun = 9.20
 dolarDun = "9.20"
+
 {
     let dolarDun = 9.10
 }
-
 console.log(dolarDun)
+// const ile bir değişken atandığında başka bir değer atanamaz.!!!
 
-const euroDun = 11.2
-//euroDun = 11
+const euroDun = 11.8
 
 console.log(euroDun)
-
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+//array 
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi","Kamu Kredi","Özel Konut Kredisi"] 
 
 console.log("<ul>")
 for(let i = 0;i<konutKredileri.length;i++){
+
     console.log("<li>"+konutKredileri[i]+"</li>")
 }
+
+
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
+console.log (konutKredileri)
